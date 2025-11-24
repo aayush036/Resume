4f870765ddc17359589412d3aa6dd727848c468b
Aayush <patelayu182002@gmail.com>
2025-11-24 09:18:27 +0530

    Refactor: reorganized folders, updated files, major structure changes

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ import express from "express";
 import nodemailer from "nodemailer"
 import contactRoutes from "./routes/contactRoutes.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use("/contact", contactRoutes);
+app.use(express.static(path.join(__dirname, "portfolio/dist")));
+app.use((_, res) => {
+    res.sendFile(path.join(__dirname, 'portfolio', 'dist', 'index.html'));
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
